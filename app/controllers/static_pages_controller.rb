09f9d2546ddbff57427aeab1b6f546f0faeaa602
Christopher Dobler <chrisdobler@gmail.com>
2016-03-04 03:07:12 -0800

    Reads Hands into two arrays for each player

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,19 @@
 class StaticPagesController < ApplicationController
 
 	def gameboard
+		file = File.open("db/hands_db.txt", "r")
+		p1 = [] # array of all hands for player 1
+		p2 = []
+		scores = []
+		i = 0
+		@contents = []
+	   	while !file.eof?
+			line = file.readline
+			cards = line.split(" ")
+			p1.push cards[0..4]
+		    p2.push cards[5..9]
 
+		   i = i+1
+		end
 	end
 end
