5c5e23dbb7f005ac7e9c56a3c9c965b87a82ec12
Christopher Dobler <chrisdobler@gmail.com>
2016-03-08 04:38:57 -0800

    Sets variables for view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,11 @@ class StaticPagesController < ApplicationController
 
 		game = ScoreController.new
 		game.parse "db/hands_db.txt"
-		@contents = game.evaluate
+		contents = game.evaluate
 
-		
+		@hands = contents[2]
+		@scores = contents[1]
+		@reasons = contents[0]
+		@totals = contents[4]
 	end
 end
