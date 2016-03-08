dca9936ebb00b6d5c2d573724d1177135a42b53c
Christopher Dobler <chrisdobler@gmail.com>
2016-03-08 04:02:07 -0800

    Display Helpers

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,27 @@
 module StaticPagesHelper
+	def to_suit letter
+		case letter
+		when "S"
+			'♠'
+		when "D"
+			'♦'
+		when "C"
+			'♣'
+		when "H"
+			'♥'
+		end
+	end
+
+	def to_suit_name letter
+		case letter
+		when "S"
+			'spades'
+		when "D"
+			'diamonds'
+		when "C"
+			'clubs'
+		when "H"
+			'hearts'
+		end
+	end
 end
